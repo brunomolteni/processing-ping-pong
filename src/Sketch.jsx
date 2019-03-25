c95781504966f9af131a0ce7f210597485fa4772
Bruno Molteni <brunomolteni90@gmail.com>
2019-03-25 22:20:53 +0100

    Added toggling code on mobile

--- src/Sketch.jsx
+++ src/Sketch.jsx
@@ -8,17 +8,21 @@ import "./Sketch.scss";
 const Sketch = ({ code, id }) => {
   const key = `pjs-${id}`;
   const canvasEl = useRef(null);
+  const codeEl = useRef(null);
+  const [isExpanded, setExpanded] = useState(false);
   var instance = null;
 
+  const toggleExpanded = () => setExpanded(!isExpanded);
+
   const compileAndRun = code => {
+    stopLoop();
     let sketch = Processing.compile(code);
     instance = new Processing(key, sketch);
     playLoop();
   };
 
   const updateCode = () => {
-    stopLoop();
-    let changedCode = canvasEl.current.textContent;
+    let changedCode = codeEl.current.textContent;
     compileAndRun(changedCode);
   };
 
@@ -34,7 +38,7 @@ const Sketch = ({ code, id }) => {
     entries.map(entry => (entry.isIntersecting ? playLoop() : stopLoop()));
 
   useEffect(() => {
-    canvasEl.current.textContent = code;
+    codeEl.current.textContent = code;
     compileAndRun(code);
     Prism.highlightAll();
   });
@@ -42,8 +46,9 @@ const Sketch = ({ code, id }) => {
   useIntersectionObserver(canvasEl, playWhenVisible);
 
   return (
-    <div className="Sketch">
-      <canvas className="Sketch__canvas" id={key} />
+    <div className={`Sketch ${isExpanded ? "--expanded" : ""}`}>
+      <h3 className="Sketch__title">Round {id + 1}</h3>
+      <canvas className="Sketch__canvas" id={key} ref={canvasEl} />
       <hr className="Sketch__separator" />
       <pre
         className="Sketch__code"
@@ -57,8 +62,9 @@ const Sketch = ({ code, id }) => {
         />
       </pre>
       <div className="Sketch__buttons">
-        <button onClick={stopLoop}>◼</button>
-        <button onClick={updateCode}>↷</button>
+        <button onClick={stopLoop}>◼ Stop</button>
+        <button onClick={updateCode}>↻ Run</button>
+        <button onClick={toggleExpanded}>↕ Code</button>
       </div>
     </div>
   );
